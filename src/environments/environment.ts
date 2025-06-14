a26ccc6bde7e7191747b5a17a64dec9be70ddf36
Julius Polar <julius.polar@protonmail.com>
2025-06-14 18:36:49 +0200

    updated env

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: "scrum-poker-cc573",
+    appId: "1:785734208890:web:cbfa30dd31f059f4ccdac3",
+    databaseURL:
+      "https://scrum-poker-cc573-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "scrum-poker-cc573.appspot.com",
+    apiKey: "AIzaSyBRwKcuZzSFG_Dvcoe0jpvofpEEKcXGOSo",
+    authDomain: "scrum-poker-cc573.firebaseapp.com",
+    messagingSenderId: "785734208890",
+  },
+};
